efbb44520a0f96c834b7972cd5be8907691fe343
caldenet <dizeoc@gmail.com>
2021-05-17 11:21:43 -0300

    289

--- src/js/app.js
+++ src/js/app.js
@@ -4,10 +4,21 @@ document.addEventListener('DOMContentLoaded', function () {
     darkMode();
 });
 
-function darkMode(){
-    const botonDarkMode = document.querySelector('.dark-mode-boton');
+function darkMode() {
+    const prefiereDarkMode = window.matchMedia('(prefers-color-scheme: dark)');
 
-    botonDarkMode.addEventListener('click', function(){
+    // console.log(prefiereDarkMode.matches);
+
+    prefiereDarkMode.addEventListener('change', function () {
+        if (prefiereDarkMode.matches) {
+            document.body.classList.add('dark-mode');
+        } else {
+            document.body.classList.remove('dark-mode');
+        }
+    });
+    
+    const botonDarkMode = document.querySelector('.dark-mode-boton');
+    botonDarkMode.addEventListener('click', function () {
         document.body.classList.toggle('dark-mode');
     });
 }
