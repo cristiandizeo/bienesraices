113fb4b290cd69b62ca10293c3f9b63105694f36
caldenet <dizeoc@gmail.com>
2021-05-18 20:37:18 -0300

    302

--- includes/templates/header.php
+++ includes/templates/header.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bienes raíces</title>
-    <link rel="stylesheet" href="build/css/app.css">
+    <link rel="stylesheet" href="/build/css/app.css">
 </head>
 
 <body>
@@ -14,12 +14,12 @@
         <div class="contenedor contenido-header">
             <div class="barra">
 
-                <a href="/"><img src="build/img/logo.svg" alt="Bienes raices"></a>
+                <a href="/"><img src="/build/img/logo.svg" alt="Bienes raices"></a>
                 <div class="mobile-menu">
-                    <img src="build/img/barras.svg" alt="icono menu responsive">
+                    <img src="/build/img/barras.svg" alt="icono menu responsive">
                 </div>
                 <div class="derecha">
-                    <img src="build/img/dark-mode.svg" alt="Dark mode" class="dark-mode-boton">
+                    <img src="/build/img/dark-mode.svg" alt="Dark mode" class="dark-mode-boton">
                 <nav class="navegacion">
                     <a href="nosotros.php">Nosotros</a>
                     <a href="anuncios.php">Anuncios</a>
