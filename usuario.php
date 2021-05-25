fed2ef2fb93016fe3f1d32fb32c99bfed8166967
caldenet <dizeoc@gmail.com>
2021-05-24 22:25:07 -0300

    327

---
+++ usuario.php
@@ -0,0 +1,13 @@
+<?php
+    require 'includes/config/database.php';
+    $db = conectarDB();
+
+    $email = "correo@correo.com";
+    $password = "123456";
+
+    $passwordHash = password_hash($password, PASSWORD_DEFAULT);
+    
+    $query = "INSERT INTO usuarios (email, password) VALUES ('${email}', '${passwordHash}')";
+
+    mysqli_query($db, $query); 
+?>
